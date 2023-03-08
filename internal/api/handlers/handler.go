f5cb89d4e076555882c258fe5978c5c1040809ad
Aitugan <mirash2001@gmail.com>
2023-03-09 00:22:21 +0600

    feat: initialized book and usre handlers in getter

--- internal/api/handlers/handler.go
+++ internal/api/handlers/handler.go
@@ -1,6 +1,22 @@
 package handlers
+
+import (
+	"github.com/Aitugan/Techt/app/usecase"
+	handlerIntf "github.com/Aitugan/Techt/internal/api/handler_interfaces"
+)
+
 type Handler struct {
+	UserHandler handlerIntf.RestUserHandler
+	BookHandler handlerIntf.RestBookHandler
+}
+
 func Get() *Handler {
+	uc := usecase.Get()
+	bookHandler := NewBookHandler(uc.UserUsecase, uc.BookUsecase)
+	userHandler := NewUserHandler(uc.UserUsecase)
+
 	return &Handler{
+		BookHandler: bookHandler,
+		UserHandler: userHandler,
 	}
 }
